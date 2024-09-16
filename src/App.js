5b58e3c3d8def25331a91a18284e267b85822eeb
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-09-16 10:06:29 +0700

    2 - made 4 pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <BrowserRouter>
       <div className='App'>
         <Routes>
-          <Route
-            path='/'
-            element={<h1>Router</h1>}></Route>
+          <Route path='/' element={<h1>Router</h1>}></Route>
+          <Route path='about' element={<h1>About</h1>}></Route>
+          <Route path='contacts' element={<h1>Contacts</h1>}></Route>
+          {/* Default route path=* */}
+          <Route path='*' element={<h1>404 Not Found</h1>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
