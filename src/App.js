1143ea966dc45a1d632bf15de9cd7ef91b33232d
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-09-16 09:33:20 +0700

    2 - made Brouser Router "div"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route
+            path='/'
+            element={<h1>Router</h1>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
