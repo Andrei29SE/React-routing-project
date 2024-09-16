1ec56aa84a0f3ed31e3803083f7277f2bb0f25bf
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-09-16 11:31:10 +0700

    4 - add fotter and some CSS

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <form>
+        <label>
+          Email:
+          <input type='email' />
+        </label>
+        <label>
+          Name:
+          <input type='text' />
+        </label>
+        <label>
+          Surname:
+          <input type='text' />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default Footer
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router'
 import Menu from '../Components/Menu'
+import Footer from '../Components/Footer'
 
 const MainLayout = () => {
   return (
     <>
-      <Menu />
+      <Menu className='top' />
       <Outlet />
+      <Footer className='bottom' />
     </>
   )
 }
