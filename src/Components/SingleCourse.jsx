4de753be533aa6550f09fb1afb57f1db66ab6c5e
Andrei Skiruta <skiruta.andrei@mail.ru>
2024-09-17 09:02:21 +0700

    7-finished sindle courses pages and navigation

--- src/Components/SingleCourse.jsx
+++ src/Components/SingleCourse.jsx
@@ -1,10 +1,20 @@
+import { Link } from 'react-router-dom'
 import courses from '../data/courses'
 import { useParams } from 'react-router'
 const SingleCourse = () => {
   const params = useParams()
   console.log(params)
-  //   const course = courses.find((course) => course.slug === params.slug)
-  return <h1>Single Course</h1>
+  const course = courses.find((course) => course.slug === params.slug)
+  return (
+    <div className='courseCard'>
+      <h1>{course.title}</h1>
+      <h2>Main language: {course.slug}</h2>
+      <h3>Price per course: {course.price}$</h3>
+      <Link to='..' relative='path'>
+        All courses
+      </Link>
+    </div>
+  )
 }
 
 export default SingleCourse
